95da103e91cee210b4643c3f7add2834bcf09664
Jun Han <formulahendry@gmail.com>
2017-08-16 10:27:41 +0800

    Add unit test (#2)

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,22 +1,54 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
+import * as assert from "assert";
+import * as vscode from "vscode";
 
-// The module 'assert' provides assertion methods from node
-import * as assert from 'assert';
+const extensionId = "formulahendry.azure-iot-toolkit";
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-import * as myExtension from '../src/extension';
-
-// Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", () => {
 
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    test("should be present", () => {
+        assert.ok(vscode.extensions.getExtension(extensionId));
+    });
+
+    // tslint:disable-next-line:only-arrow-functions
+    test("should be able to activate the extension", function (done) {
+        this.timeout(60 * 1000);
+        const extension = vscode.extensions.getExtension(extensionId);
+        if (!extension.isActive) {
+            extension.activate().then((api) => {
+                done();
+            }, () => {
+                done("Failed to activate extension");
+            });
+        } else {
+            done();
+        }
+    });
+
+    test("should be able to register iot toolkit commands", () => {
+        return vscode.commands.getCommands(true).then((commands) => {
+            const COMMANDS = [
+                "azure-iot-toolkit.refreshDeviceTree",
+                "azure-iot-toolkit.getDevice",
+                "azure-iot-toolkit.sendD2CMessage",
+                "azure-iot-toolkit.startMonitorIoTHubMessage",
+                "azure-iot-toolkit.stopMonitorIoTHubMessage",
+                "azure-iot-toolkit.sendC2DMessage",
+                "azure-iot-toolkit.startMonitorC2DMessage",
+                "azure-iot-toolkit.stopMonitorC2DMessage",
+                "azure-iot-toolkit.listDevice",
+                "azure-iot-toolkit.createDevice",
+                "azure-iot-toolkit.deleteDevice",
+                "azure-iot-toolkit.invokeDeviceMethod",
+                "azure-iot-toolkit.getDeviceTwin",
+                "azure-iot-toolkit.updateDeviceTwin",
+            ].sort();
+
+            const foundCommands = commands.filter((value) => {
+                return value.startsWith("azure-iot-toolkit.");
+            }).sort();
+
+            const errorMsg = "Some iot toolkit commands are not registered properly or a new command is not added to the test";
+            assert.equal(JSON.stringify(foundCommands), JSON.stringify(COMMANDS), errorMsg);
+        });
     });
 });
