5fad66e8b2abddf199c910190b0ed91cfdec019e
formulahendry <formulahendry@gmail.com>
2017-09-26 10:54:28 +0800

    Revert "Remove Extension Dependencies when running unit test"
    This reverts commit 78ddd4387d6687d87a5271d6ca97e92a2746ce28.

--- CI/restoreExtensionDependencies.js
+++
@@ -1,4 +0,0 @@
-const fs = require("fs");
-const packageJson = JSON.parse(fs.readFileSync("package.json"));
-packageJson.extensionDependencies = ["ms-vscode.azure-account"];
-fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2) + "\n");
