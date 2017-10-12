42cfcb7709850610d0af4311ec779cf57f5bd1a6
formulahendry <formulahendry@gmail.com>
2017-10-12 16:18:22 +0800

    Draft of Edge Device Deployment

---
+++ src/iotEdgeExplorer.ts
@@ -0,0 +1,65 @@
+"use strict";
+import axios from "axios";
+import * as iothub from "azure-iothub";
+import * as fs from "fs";
+import * as vscode from "vscode";
+import { BaseExplorer } from "./baseExplorer";
+import { Constants } from "./constants";
+import { DeviceItem } from "./Model/DeviceItem";
+import { TelemetryClient } from "./telemetryClient";
+import { Utility } from "./utility";
+
+export class IoTEdgeExplorer extends BaseExplorer {
+    constructor(outputChannel: vscode.OutputChannel) {
+        super(outputChannel);
+    }
+
+    public async createDeployment(deviceItem: DeviceItem) {
+        let iotHubConnectionString = await Utility.getConnectionString(Constants.IotHubConnectionStringKey, Constants.IotHubConnectionStringTitle);
+        if (!iotHubConnectionString) {
+            return;
+        }
+
+        const deploymentJson = await this.getDeploymentJson();
+        if (!deploymentJson) {
+            return;
+        }
+        const sasToken = Utility.generateSasTokenForService(iotHubConnectionString);
+        const hostName = Utility.getHostName(iotHubConnectionString);
+
+        this.deploy(hostName, deviceItem.deviceId, sasToken, deploymentJson);
+    }
+
+    private async getDeploymentJson(): Promise<string> {
+        const options: vscode.OpenDialogOptions = {
+            openLabel: "Select Deployment File",
+        };
+        const filePathUri = await vscode.window.showOpenDialog(options);
+        if (!filePathUri) {
+            return "";
+        }
+        const filePath = filePathUri[0].fsPath;
+        return fs.readFileSync(filePath, "utf8");
+    }
+
+    private deploy(hostName: string, deviceId: string, sasToken: string, deploymentJson: string) {
+        const label = "Edge";
+        this._outputChannel.show();
+        this.outputLine(label, `Start deployment to [${deviceId}]`);
+
+        const config = {
+            headers: {
+                Authorization: sasToken,
+            },
+        };
+        const url = `https://${hostName}/devices/${deviceId}/applyConfigurationContent?api-version=2016-02-03`;
+        axios.post(url, deploymentJson, config)
+            .then((response) => {
+                this.outputLine(label, "Deployment succeeded.");
+                console.log(response);
+            })
+            .catch((err) => {
+                this.outputLine(label, `Deployment failed. ${err}`);
+            });
+    }
+}
