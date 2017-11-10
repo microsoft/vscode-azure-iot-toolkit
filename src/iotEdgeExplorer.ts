e8dbcce2cc7ff168839f99ff536827f2cb575f92
Ray Fang <fangzh@microsoft.com>
2017-11-10 14:39:32 +0800

    Update config template

--- src/iotEdgeExplorer.ts
+++ src/iotEdgeExplorer.ts
@@ -82,17 +82,20 @@ export class IoTEdgeExplorer extends BaseExplorer {
             return;
         }
 
-        const configContent: string = this.generateEdgeLaunchConfigContent(deviceItem.connectionString);
-        const configPath: vscode.Uri = await vscode.window.showSaveDialog({
-            defaultUri: vscode.workspace.workspaceFolders ? vscode.Uri.file(path.join(vscode.workspace.workspaceFolders[0].uri.fsPath, "launchConfig.json")) : undefined,
-            saveLabel: "Save Edge launch configuration file",
-            filters: {
-                JSON: ["json"],
-            },
-        });
+        const containerOS: string = await vscode.window.showQuickPick(["Linux", "Windows"], { placeHolder: "Select container OS", ignoreFocusOut: true });
+        if (containerOS) {
+            const configContent: string = this.generateEdgeLaunchConfigContent(deviceItem.connectionString, containerOS);
+            const configPath: vscode.Uri = await vscode.window.showSaveDialog({
+                defaultUri: vscode.workspace.workspaceFolders ? vscode.Uri.file(path.join(vscode.workspace.workspaceFolders[0].uri.fsPath, "launchConfig.json")) : undefined,
+                saveLabel: "Save Edge launch configuration file",
+                filters: {
+                    JSON: ["json"],
+                },
+            });
 
-        if (configPath) {
-            Utility.writeFile(configPath, configContent);
+            if (configPath) {
+                Utility.writeFile(configPath, configContent);
+            }
         }
     }
 
@@ -152,41 +155,36 @@ export class IoTEdgeExplorer extends BaseExplorer {
             });
     }
 
-    private generateEdgeLaunchConfigContent(connectionString: string): string {
+    private generateEdgeLaunchConfigContent(connectionString: string, containerOS: string): string {
         return `{
-    "schemaVersion": "1",
-    "deviceConnectionString": "${connectionString}",
-    "homeDir": "${path.join(os.homedir(), ".azure_iot_edge").replace(/\\/g, "\\\\")}",
-    "hostName": "${os.hostname()}",
-    "logLevel": "info",
-    "security": {
-        "certificates": {
-            "option": "selfSigned",
-            "selfSigned": {
-                "forceRegenerate": false,
-                "forceNoPasswords": true
-            },
-            "preInstalled": {
-                "deviceCACertificateFilePath": "",
-                "serverCertificateFilePath": ""
-            }
-        }
-    },
     "deployment": {
-        "type": "docker",
         "docker": {
-            "uri": "unix:///var/run/docker.sock",
-            "edgeRuntimeImage": "",
-            "registries": [{
-                "address": "",
-                "username": "",
-                "password": ""
-            }],
+            "edgeRuntimeImage": "microsoft/azureiotedge-agent:1.0-preview",
             "loggingOptions": {
                 "log-driver": "json-file",
                 "log-opts": {
                     "max-size": "10m"
                 }
+            },
+            "uri": "${containerOS === "Linux" ? "unix:///var/run/docker.sock" : "npipe://./pipe/docker_engine"}"
+        },
+        "type": "docker"
+    },
+    "deviceConnectionString": "${connectionString}",
+    "homeDir": "${path.join(os.homedir(), "azure_iot_edge").replace(/\\/g, "\\\\")}",
+    "hostName": "${os.hostname()}",
+    "logLevel": "info",
+    "schemaVersion": "1",
+    "security": {
+        "certificates": {
+            "option": "selfSigned",
+            "preInstalled": {
+                "deviceCACertificateFilePath": "",
+                "serverCertificateFilePath": ""
+            },
+            "selfSigned": {
+                "forceNoPasswords": true,
+                "forceRegenerate": false
             }
         }
     }
@@ -199,49 +197,40 @@ export class IoTEdgeExplorer extends BaseExplorer {
         "$edgeAgent": {
             "properties.desired": {
                 "schemaVersion": "1.0",
-                "runtime": { // Information about the edge device runtime, common to all modules
-                    "type": "docker", // Enum - values - docker
+                "runtime": {
+                    "type": "docker",
                     "settings": {
-                        "minDockerVersion": "v1.13", // Min docker version required by this edgeAgent (currently ignored)
-                        "loggingOptions": "" // Logging options for the Edge modules
+                        "minDockerVersion": "v1.25",
+                        "loggingOptions": ""
                     }
                 },
                 "systemModules": {
-                    "edgeAgent": { // System Module - required
-                        "type": "docker", // Enum - values - docker
+                    "edgeAgent": {
+                        "type": "docker",
                         "settings": {
-                            "image": "edgepreview.azurecr.io/azureiotedge/edge-agent:1.0.0-preview003", // Location of the edgeAgent image
-                            "createOptions": "" // Optional - will be null in general case. This is for future proofing
-                        },
-                        "configuration": {
-                            "id": "1234"
+                            "image": "microsoft/azureiotedge-agent:1.0-preview",
+                            "createOptions": ""
                         }
                     },
-                    "edgeHub": { // System Module - required
-                        "type": "docker", // Enum - values – docker
-                        "status": "running", // Enum - values - running, stopped
-                        "restartPolicy": "always", // This value should be "always"
+                    "edgeHub": {
+                        "type": "docker",
+                        "status": "running",
+                        "restartPolicy": "always",
                         "settings": {
-                            "image": "edgepreview.azurecr.io/azureiotedge/edge-hub:1.0.0-preview003", // Path to the EdgeHub Image
-                            "createOptions": "" // Options string to start the Edge Hub module (templated)
-                        },
-                        "configuration": {
-                            "id": "1234"
+                            "image": "microsoft/azureiotedge-hub:1.0-preview",
+                            "createOptions": ""
                         }
                     }
                 },
                 "modules": {
-                    "tempSensor": {
+                    "SampleModule": {
                         "version": "1.0",
                         "type": "docker",
                         "status": "running",
                         "restartPolicy": "always",
                         "settings": {
-                            "image": "edgepreview.azurecr.io/azureiotedge/simulated-temperature-sensor:1.0.0-preview003",
-                            "createOptions": ""
-                        },
-                        "configuration": {
-                            "id": "1234"
+                            "image": "<dockeruser>/<image>:<tag>",
+                            "createOptions": "{}"
                         }
                     }
                 }
@@ -250,12 +239,11 @@ export class IoTEdgeExplorer extends BaseExplorer {
         "$edgeHub": {
             "properties.desired": {
                 "schemaVersion": "1.0",
-                "routes": { // List of routes for the edge hub
-                    // Route for the edge hub. Route name is used as key for the routes. To delete a route, set the routeName = null
-                    "route1": "FROM /* INTO $upstream" // Sample value. Route name can be anything
+                "routes": {
+                    "route": "FROM /* INTO $upstream"
                 },
                 "storeAndForwardConfiguration": {
-                    "timeToLiveSecs": 90000 // Sample value
+                    "timeToLiveSecs": 10
                 }
             }
         }
