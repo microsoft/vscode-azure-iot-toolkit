be869e2c8ff4a25793fa417068249b1ed08911cf
Ray Fang <fangzh@microsoft.com>
2017-11-29 17:36:44 +0800

    Remove unnecessary code

--- src/Model/DeviceItem.ts
+++ src/Model/DeviceItem.ts
@@ -1,7 +1,6 @@
 import { Command, QuickPickItem, TreeItem } from "vscode";
 
 export class DeviceItem extends TreeItem implements QuickPickItem {
-    public readonly label: string;
     public readonly description: string;
     constructor(
         public readonly deviceId: string,
@@ -10,6 +9,5 @@ export class DeviceItem extends TreeItem implements QuickPickItem {
         public readonly command: Command) {
         super(deviceId);
         this.contextValue = "device";
-        this.label = deviceId;
     }
 }
