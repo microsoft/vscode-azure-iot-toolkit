a22db17ede2e9a81a2ed22c11eddddfc1bc3e48b
Ray Fang <fangzh@microsoft.com>
2017-11-30 13:33:47 +0800

    Minor refinement

--- src/Model/DeviceItem.ts
+++ src/Model/DeviceItem.ts
@@ -1,12 +1,12 @@
 import { Command, QuickPickItem, TreeItem } from "vscode";
 
 export class DeviceItem extends TreeItem implements QuickPickItem {
-    public readonly description: string;
     constructor(
         public readonly deviceId: string,
         public readonly connectionString: string,
         public readonly iconPath: string,
-        public readonly command: Command) {
+        public readonly command: Command,
+        public readonly description: string) {
         super(deviceId);
         this.contextValue = "device";
     }
