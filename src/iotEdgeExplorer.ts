e5355b86423e27ac14aff074682cfad3205db7f8
Ray Fang <fangzh@microsoft.com>
2017-12-22 11:09:33 +0800

    Update non-Windows Edge home dir

--- src/iotEdgeExplorer.ts
+++ src/iotEdgeExplorer.ts
@@ -239,7 +239,7 @@ export class IoTEdgeExplorer extends BaseExplorer {
         "type": "docker"
     },
     "deviceConnectionString": "${connectionString}",
-    "homeDir": "${(os.platform() === "win32" ? path.join(path.join(process.env.PROGRAMDATA, "azure-iot-edge"), "data") : path.join("/etc", "azure_iot_edge")).replace(/\\/g, "\\\\")}",
+    "homeDir": "${(os.platform() === "win32" ? path.join(process.env.PROGRAMDATA, "azure-iot-edge", "data") : path.join("/var", "lib", "azure_iot_edge")).replace(/\\/g, "\\\\")}",
     "hostName": "${fqdn().toLowerCase()}",
     "logLevel": "info",
     "schemaVersion": "1",
