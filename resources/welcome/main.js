1be82dd8d93dc33ac49935f1fdc315b548f88bd0
Jun Han <junhan@microsoft.com>
2018-07-16 17:13:58 +0800

    update

--- resources/welcome/main.js
+++ resources/welcome/main.js
@@ -3,7 +3,7 @@ $(function () {
 });
 
 function main() {
-    //const vscode = acquireVsCodeApi();
+    // const vscode = acquireVsCodeApi();
     $("a").click((event) => {
         console.log(event.target.href);
         vscode.postMessage({
@@ -14,7 +14,7 @@ function main() {
     $(".interactive").click((event) => {
         console.log(event);
         if (!$(event.target).is("a") && !$(event.target).is(".detail") &&
-            !$(event.target).parents(".detail").length > 0) {
+            $(event.target).parents(".detail").length === 0) {
             $(event.currentTarget).find(".detail").toggle(1000);
             $(event.currentTarget).find(".arrow").toggleClass("arrow-up")
         }
