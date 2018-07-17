a9970d377241d87c5dba6bf849e11da79c59ad7e
Jun Han <junhan@microsoft.com>
2018-07-17 16:15:02 +0800

    Refine

--- resources/welcome/main.js
+++ resources/welcome/main.js
@@ -3,20 +3,21 @@ $(function () {
 });
 
 function main() {
-    // const vscode = acquireVsCodeApi();
+    const vscode = acquireVsCodeApi();
     $("a").click((event) => {
-        console.log(event.target.href);
         vscode.postMessage({
             href: event.target.href
-        })
+        });
     });
 
     $(".interactive").click((event) => {
-        console.log(event);
         if (!$(event.target).is("a") && !$(event.target).is(".detail") &&
             $(event.target).parents(".detail").length === 0) {
             $(event.currentTarget).find(".detail").toggle(1000);
-            $(event.currentTarget).find(".arrow").toggleClass("arrow-up")
+            $(event.currentTarget).find(".arrow").toggleClass("arrow-up");
+            vscode.postMessage({
+                href: `toggle:${$(event.currentTarget).attr("id")}`
+            });
         }
     });
 
