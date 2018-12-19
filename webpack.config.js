0514d3e5fba7b870313652e9e2279a1744753bb0
Ray Fang <fangzh@microsoft.com>
2018-12-19 17:42:01 +0800

    Add commonjs prefix

--- webpack.config.js
+++ webpack.config.js
@@ -28,9 +28,9 @@ const config = {
     devtool: 'source-map',
     externals: {
         vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
-        bufferutil: 'bufferutil',   // bufferutil and utf-8-validate are actually optional binary dependencies. Adding them to suppress the warning https://github.com/websockets/ws/issues/719
-        'utf-8-validate': 'utf-8-validate',
-        'spawn-sync': 'spawn-sync'  // spawn-sync is only required for Node.js <= 0.12 https://github.com/ForbesLindesay/spawn-sync
+        bufferutil: 'commonjs bufferutil',   // bufferutil and utf-8-validate are actually optional binary dependencies. Adding them to suppress the warning https://github.com/websockets/ws/issues/719
+        'utf-8-validate': 'commonjs utf-8-validate',
+        'spawn-sync': 'commonjs spawn-sync'  // spawn-sync is only required for Node.js <= 0.12 https://github.com/ForbesLindesay/spawn-sync
     },
     resolve: { // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
         extensions: ['.ts', '.js']
