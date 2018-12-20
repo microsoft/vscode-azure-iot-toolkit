018d5c6639ad5d7c82842f9be9e133beb564a439
Ray Fang <fangzh@microsoft.com>
2018-12-20 17:14:07 +0800

    Use Webpack to improve extension performance (#226)

---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+//@ts-check
+
+'use strict';
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const failOnErrorsPlugin = require('fail-on-errors-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+    target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+    node: {
+        __dirname: false
+    },
+    entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    output: { // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+        path: path.resolve(__dirname, 'dist', 'scripts'),   // the clipboardy package ships with binaries in the fallbacks folder and reference them with "../fallbacks/*"
+        filename: 'extension.js',
+        libraryTarget: "commonjs2",
+        devtoolModuleFilenameTemplate: "../[resource-path]",
+    },
+    devtool: 'source-map',
+    externals: {
+        vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+        bufferutil: 'commonjs bufferutil',   // bufferutil and utf-8-validate are actually optional binary dependencies. Adding them to suppress the warning https://github.com/websockets/ws/issues/719
+        'utf-8-validate': 'commonjs utf-8-validate',
+        'spawn-sync': 'commonjs spawn-sync'  // spawn-sync is only required for Node.js <= 0.12 https://github.com/ForbesLindesay/spawn-sync
+    },
+    resolve: { // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'ts-loader',
+                }]
+            },
+            {
+                test: /node_modules[/\\]mqtt[/\\]mqtt\.js$/i,   // remove the #! which cannot be handled by Webpack
+                use: [{
+                    loader: 'shebang-loader'
+                }]
+            },
+            {
+                test: /node_modules[/\\]mqtt[/\\]bin[/\\][ps]ub\.js$/i, // remove the #! which cannot be handled by Webpack
+                use: [{
+                    loader: 'shebang-loader'
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([{
+            from: 'node_modules/clipboardy/fallbacks',
+            to: '../fallbacks'  // copy clipboardy binaries to the parent folder of scripts
+        }]),
+        // Ignore all locale files of moment.js, which can save 50KB
+        // https://webpack.js.org/plugins/ignore-plugin/#ignore-moment-locales
+        new webpack.IgnorePlugin(/^\.\/locale$/, /[\/\\]moment$/),
+        // Suppress warnings of known dynamic require
+        new webpack.ContextReplacementPlugin(
+            /applicationinsights[\/\\]out[\/\\]AutoCollection/,
+            false,
+            /$^/
+        ),
+        new webpack.ContextReplacementPlugin(
+            /ms-rest[\/\\]lib/,
+            false,
+            /$^/
+        ),
+        new webpack.ContextReplacementPlugin(
+            /applicationinsights[\/\\]out[\/\\]Library/,
+            false,
+            /$^/
+        ),
+        // Pack node_modules/getos/logic/*.js
+        new webpack.ContextReplacementPlugin(
+            /getos/,
+            /logic[\/\\].*\.js/
+        ),
+        // Fail on warnings so that CI can report new warnings which require attention
+        new failOnErrorsPlugin({
+            failOnErrors: true,
+            failOnWarnings: true,
+        })
+    ]
+}
+
+module.exports = config;
