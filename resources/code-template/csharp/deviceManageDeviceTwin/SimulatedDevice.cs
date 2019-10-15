a85386802b8303cfb36ec143e4822ae6da33cd31
tomasz.bartoszewski <tomasz.bartoszewski@gmail.com>
2019-10-15 19:27:09 +0100

    (GH-182) Add C# code generation for device manage device twin

---
+++ resources/code-template/csharp/deviceManageDeviceTwin/SimulatedDevice.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using System.Threading.Tasks;
+
+namespace simulatedDevice
+{
+    class SimulatedDevice
+    {
+        static string DeviceConnectionString = "{{deviceConnectionString}}";
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Connecting to hub");
+                DeviceClient Client = DeviceClient.CreateFromConnectionString(DeviceConnectionString, 
+                    TransportType.Mqtt);
+                Console.WriteLine("Retrieving twin");
+                var twin = await Client.GetTwinAsync();
+                await Client.SetDesiredPropertyUpdateCallbackAsync(OnDesiredPropertyChanged, null);
+                
+                var reportedProperties = new TwinCollection();
+                var weather = new TwinCollection();
+                weather["temperature"] = 72;
+                weather["humidity"] = 17;
+                reportedProperties["firmwareVersion"] = "1.2.2";
+                reportedProperties["weather"] = weather;
+
+                await Client.UpdateReportedPropertiesAsync(reportedProperties);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Error in sample: {0}", ex.Message);
+            }
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+
+        private static async Task OnDesiredPropertyChanged(TwinCollection desiredProperties, object userContext)
+        {
+            await Task.Run(() => {
+                Console.WriteLine("New desired properties received:");
+                Console.WriteLine(desiredProperties.ToJson());
+            });
+        }
+    }
+}
