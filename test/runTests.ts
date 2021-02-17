4dddf9fdd528c0e01d776b67ffe05c9f9bc93cfd
Ryan Kelly <rykelly@microsoft.com>
2021-02-17 14:08:07 -0800

    packaging and testing updates
    updated package vulnerabilities
    
    updated vscode -> vscode-test
    
    updated version to 2.16.6-rc

---
+++ test/runTests.ts
@@ -0,0 +1,24 @@
+import { resolve } from "path";
+
+import { runTests } from "vscode-test";
+
+async function main() {
+    try {
+        // The folder containing the Extension Manifest package.json
+        // Passed to `--extensionDevelopmentPath`
+        const extensionDevelopmentPath = resolve(__dirname, "../");
+
+        // The path to the extension test script
+        // Passed to --extensionTestsPath
+        const extensionTestsPath = resolve(__dirname, ".");
+
+        // Download VS Code, unzip it and run the integration test
+        await runTests({ extensionDevelopmentPath, extensionTestsPath });
+    } catch (err) {
+        // tslint:disable-next-line: no-console
+        console.error("Failed to run tests");
+        process.exit(1);
+    }
+}
+
+main();
